db7f8984186340697bf1ec1ef7c6d69ecc777fb5
delaxeii <52194855+delaxeii@users.noreply.github.com>
2020-03-04 10:11:19 +0300

    fixed

--- task2.py
+++ task2.py
@@ -1,4 +1,4 @@
-keys= str(input("Enter keys through spaces \n: "))
+keys = str(input("Enter keys through spaces \n: "))
 listkeys = keys.split(" ")
 values = str(input("Enter values through spaces \n: "))
 listval = values.split(" ")
