1fa1d751793a7a87ac0db1d8709297809b0beef2
delaxeii <52194855+delaxeii@users.noreply.github.com>
2020-03-04 10:03:13 +0300

    fixed

--- task3.py
+++ task3.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
-
 n = int(input("Enter number \n: "))
 fact = 1
 
-for i in range(1, n + 1):
+for i in range(1,n+1):
     fact = fact * i
 
-print ("The factorial is : ", end="")
+print ("The factorial is : ",end="")
 print (fact)
