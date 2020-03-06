9aba993ab5047bf376a98d94a3d17339bc15a0e3
delaxeii <52194855+delaxeii@users.noreply.github.com>
2020-03-06 08:40:35 +0300

    fixed

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import os
 import argparse
 from datetime import datetime
@@ -13,19 +14,27 @@ parser.add_argument("ext", "--extension", help="Filter by file extension")
 parser.add_argument("-v", action="version", version="Version 1.0, by Andrei Leanovich")
 
 args = parser.parse_args()
+folder = args.dir
+l1 = []
 
-if args.directory:
-    for entry in os.listdir(args.path):
-        if os.path.isfile(os.path.join(args.path, entry)):
-            print(entry)
 
-if args.recursive:
-    list_files(args.path)
-else:
-    with os.scandir(args.path) as entries:
+def list_files_rec(path):
+    with os.scandir(path) as entries:
         for entry in entries:
             if entry.is_file():
-                print([entry.name, os.path.getctime(entry)])
+                l1.append([entry.name, entry.stat().st_ctime])
+            elif entry.is_dir():
+                list_files_rec(entry.path)
+    return l1
+
+
+def list_files_par(path):
+    with os.scandir(path) as entries:
+        for entry in entries:
+            if entry.is_file():
+                l1.append([entry.name, entry.stat().st_ctime])
+    return l1
+
 
 if args.extention:
     for root, dirs, files in os.walk(args.path):
@@ -38,7 +47,7 @@ if args.sortname:
     for i in range(len(s)):
         print(s[i])
 
-def get_date(timestamp):
-    d = datetime.utcfromtimestamp(timestamp)
-    formated_date = d.strftime('%Y-%m-%d')
-    return formated_date
+        def get_date(timestamp):
+            d = datetime.utcfromtimestamp(timestamp)
+            formated_date = d.strftime('%Y-%m-%d')
+            return formated_date
